4a824bd78b90d3ee9b7fa8ba4c20fbf9b048a4ed
GaiaGD <gaia.digregorio@gmail.com>
2022-12-21 20:01:37 -0500

    pffff

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -98,7 +98,7 @@ function Homepage() {
       </div>
 
       {/* endorsement */}
-      <div className='flex justify-between items-center mx-4'>
+      {/* <div className='flex justify-between items-center mx-4'>
         <div className='md:mt-64 md:mb-32 md:mx-64 md:flex flex-col items-center'>
           <h1 className={`${planet.first} md:text-4xl text-xl text-center md:text-left uppercase my-8`}>what they say about me</h1>
           <div className='endorsement flex-col justify-end'>
@@ -106,21 +106,26 @@ function Homepage() {
             <p className={`md:text-right text-center ${planet.second} my-8`}>Someone Lastname, Working as</p>
           </div>
         </div>
-      </div>
+      </div> */}
 
 
       {/* planet info */}
       <div className='planet-info h-[50vh]'>
-        <div className='relative overflow-x-clip h-48'>
+        <div className='relative overflow-x-clip h-48 z-10'>
           <div className='absolute w-1/3 -right-48'>
             <img src={`src/assets/img/${planet.selected}-zoom.png`}/>
           </div>
         </div>
 
-        <div className='relative w-full'>
-          <div className='absolute right-1/4 left-1/4 w-2/4'>
-            <h2 className={`text-right text-xl ${planet.second}`}>Mars can have huge dust storms which last for months and can cover the entire planet</h2>
+        <div className='relative w-full h-48 flex'>
+          <div className="scroll-container border-t-[0.1px] border-b-[0.1px] border-slate-400 border-solid py-3 w-full">
+            <div className="scroll-text">
+                <h2 className='text-xl font-light'>Mars can have huge dust storms which last for months and can cover the entire planet. Mars has 2 moons called Deimos and Phobos. They are named after the two horses that pull the Roman god of war, Mars’, chariot. They may be asteroids captured by Mars’ gravity.</h2>
+            </div>
           </div>
+          {/* <div className='absolute right-1/4 left-1/4 w-2/4'>
+            <h2 className={`text-right text-xl font-light ${planet.second}`}>Mars can have huge dust storms which last for months and can cover the entire planet</h2>
+          </div> */}
         </div>
       </div>
 
